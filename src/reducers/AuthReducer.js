6ea00fb5952e119f28c339db56e08c90822770c8
Palagummi <paladurg@amazon.com>
2017-12-14 13:31:06 -0800

    Chapter 'Handling Data in React vs Redux' Complete * AuthReducer is ready to catch (listen) type email_changed

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,11 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case '':
-      break;
+    case EMAIL_CHANGED:
+      
     default:
       return state;
   }
