be6d4baa28f05f57d455dc1ce218fe0e942b1776
Palagummi <paladurg@amazon.com>
2017-12-14 13:20:05 -0800

    Chapter 'Handling Data in React vs Redux' Checkpoint * Rebuild LoginForm and make it redux-ready * Handle form updates with Action Creators * Wire up the Action Creators

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = { email: '' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case '':
+      break;
+    default:
+      return state;
+  }
+};
