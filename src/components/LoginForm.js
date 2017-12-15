d088a19bd879075ba1e68a5dc840c7ce777dff10
Palagummi <paladurg@amazon.com>
2017-12-14 16:09:54 -0800

    Chapter 'Dont Mutate That State' Checkpoint * Wire up Action Creator for password text changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
   }
 
   render() {
@@ -16,12 +20,18 @@ class LoginForm extends Component {
             label="email"
             placeholder="email@gmail.com"
             onChangeText={this.onEmailChange.bind(this)}
-            value={this.props.value}
+            value={this.props.email}
           />
         </CardSection>
 
         <CardSection>
-          <Input secureTextEntry label="password" placeholder="password" />
+          <Input
+            secureTextEntry
+            label="password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
         </CardSection>
 
         <CardSection>
@@ -32,10 +42,11 @@ class LoginForm extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
