7708970fb9e38014ba5378d929b3601ce19b5ab4
Palagummi <paladurg@amazon.com>
2017-12-19 10:44:26 -0800

    Chapter 'Navigating Users Around' Checkpoint * Make EmployeeCreate form * Refactor Actions/index and move stuff to AuthActions

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,9 +1,16 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      return { ...state, [action.payload.prop]: action.payload.value };
+    /*
+     * [action.payload.prop] is NOT an array
+     * is "key-interpolation" here [action.payload.prop] is name
+     */
     default:
       return state;
   }
