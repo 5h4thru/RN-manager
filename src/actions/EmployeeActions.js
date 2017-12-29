150ceb742f44901875bcf89210e5583067483b10
Palagummi <paladurg@amazon.com>
2017-12-29 13:14:21 -0800

    Chapter 'Firebase As Data Store' Checkpoint * Redux Thunk dispatch to fetch employee list

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -11,7 +11,6 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
-
   return dispatch => {
     firebase
       .database()
@@ -22,6 +21,18 @@ export const employeeCreate = ({ name, phone, shift }) => {
           type: EMPLOYEE_CREATE
         });
         Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
       });
   };
 };
