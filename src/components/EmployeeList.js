a5465b0abcba2b938db8ae8e62086254b486775a
Palagummi <paladurg@amazon.com>
2017-12-29 13:56:21 -0800

    Chapter 'Firebase As Data Store' Checkpoint * Dynamic DataSource building

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  /*
+   * Lifecycle method that is triggered when component receives new set of mapStateToProps
+   */
+  componentWillReceiveProps(nextProps) {
+    // nextProps is the next set of props that this component will be rendered
+    // this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
     return (
       <View>
@@ -17,4 +41,4 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
