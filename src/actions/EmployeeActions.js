655c57069f499430e7f21881d3a4ed8dbda7d645
Palagummi <paladurg@amazon.com>
2017-12-29 12:01:17 -0800

    Chapter 'Firebase As Data Store' Checkpoint * Create employee and store it into firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,6 @@
+import firebase from 'firebase';
 import { EMPLOYEE_UPDATE } from './types';
+
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -8,5 +10,9 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth();
+  firebase
+    .database()
+    .ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 };
